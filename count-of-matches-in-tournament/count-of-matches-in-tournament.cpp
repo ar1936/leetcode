dc9a3206e220434cb8eb7d7c04d55561f8cfb2d0
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-03 17:28:08 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.86%) - LeetHub

---
+++ count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if(n==1)
+            return 0;
+        if(n==2)
+            return 1;
+        if(n%2==0)
+        return numberOfMatches(n/2)+n/2;
+        else 
+        return numberOfMatches(n-1)/2+(n-1)/2+1;
+        return 0;
+        
+    }
+};
