bb89995a9a02464f1ce4359cfae7aad11f4509a3
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-08 14:05:25 +0530

    Time: 4 ms (75.62%), Space: 8.2 MB (51.60%) - LeetHub

---
+++ unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int>mp;
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++;
+            
+            
+        } 
+        set<int>s;
+        for(int i=0;i<arr.size();i++)
+        {
+           
+                s.insert(mp[arr[i]]);
+        }
+        if(s.size()==mp.size())
+        return true;
+        return false;
+    }
+};
