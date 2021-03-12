0544b4bcfb412f16d73c32bcf24d08e4a0823a35
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-12 12:16:29 +0530

    Time: 28 ms (90.52%), Space: 13 MB (92.39%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,19 +1,50 @@
+// class Solution {
+// public:
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         int n=matrix.size();
+//         int i=0,j=n-1,count=0;
+//         priority_queue <int> pq;
+//             for(int i=0;i<n;i++)
+//             {
+//                 for(int j=0;j<n;j++)
+//                 {
+//                     pq.push(matrix[i][j]);
+//                     if(pq.size()>k)
+//                         pq.pop();
+//                 }
+//             }
+//         return pq.top();
+        
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n=matrix.size();
-        int i=0,j=n-1,count=0;
-        priority_queue <int> pq;
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    pq.push(matrix[i][j]);
-                    if(pq.size()>k)
-                        pq.pop();
+    int kthSmallest(vector<vector<int>>& a, int k) {
+        int rows = a.size();
+        int cols = a[0].size();
+        int start = a[0][0];
+        int end = a[rows-1][cols-1];
+        while(start<end) {
+            int mid = (start+end)/2;
+            int j = cols-1;
+            int cnt = 0;
+            for (int i = 0; i<rows; i++) {
+                while(j>=0 and a[i][j] > mid) {
+                    j--;
                 }
+                cnt += (j+1);
             }
-        return pq.top();
-        
+            if (cnt < k) {
+                start = mid+1;
+            }
+            else {
+                end = mid;
+            }
+        }
+        return start;
     }
 };
