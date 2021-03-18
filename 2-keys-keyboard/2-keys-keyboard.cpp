5cdbd53d30ce412d8d45348669b90a4f4b32399c
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-18 17:15:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (76.98%) - LeetHub

--- 2-keys-keyboard/2-keys-keyboard.cpp
+++ 2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    int minSteps(int n) {
-        if(n == 1)
-            return 0;
-        return primeFactors(n);
-            
-    }
     
-    int primeFactors(int n) {
-        int sum = 0;
-        while(n % 2 == 0) {
-            sum += 2;
-            n /= 2;
+    int minSteps(int n) {
+        int res=0,k=2;
+        while(n>1){
+            while(n%k==0)res+=k,n/=k;
+            k++;
         }
-        
-        for(int i=3;i<=n;i+=2) {
-            while(n % i == 0) {
-                sum += i;
-                n /= i;
-            } 
-        }
-        return sum;
+        return res;
     }
 };
