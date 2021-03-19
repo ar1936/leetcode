7846c52cf2dc5117580b000ec83f5b71ff71eada
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-19 19:41:53 +0530

    Time: 8 ms (91.13%), Space: 6.9 MB (97.74%) - LeetHub

--- coin-change-2/coin-change-2.cpp
+++ coin-change-2/coin-change-2.cpp
@@ -38,10 +38,38 @@ public:
         int dp[amount + 1];
         memset(dp,0,sizeof(dp));
         dp[0] = 1;
-        for (const auto & c : coins) 
-            for (int i = c; i <= amount; ++i) 
-                dp[i] += dp[i - c];    
+//         for (const auto  c : coins) 
+//             for (int i = c; i <= amount; ++i) 
+//                 dp[i] += dp[i - c];    
+        
+        
+        for (int i=1; i<=coins.size();i++) {
+            for (int j=coins[i-1]; j<=amount;j++) {
+                // if (i >=coins[j-1]) 
+                    dp[j] += dp[j - coins[i-1]];
+            }
+        }
+        
         
         return dp[amount];
     }
 };
+
+// class Solution {
+// public:
+//      int combinationSum4(vector<int>& nums, int target) {
+//         unsigned int t[target+1];
+//         memset(t,0,sizeof(t));
+//         t[0] = 1;
+//         for (int i = 1; i <= target; ++i) {
+//             for (int j=1; j<=nums.size();j++) {
+//                 if (i >=nums[j-1]) 
+//                     t[i] += t[i - nums[j-1]];
+//             }
+//         }
+        
+//         return t[target];
+//     }
+    
+// };
+
