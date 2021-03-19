dfea86ae032134a33f1d41d90944f7b8884035de
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-19 19:22:45 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (99.14%) - LeetHub

--- combination-sum-iv/combination-sum-iv.cpp
+++ combination-sum-iv/combination-sum-iv.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
      int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int> result(target + 1);
-        result[0] = 1;
+        unsigned int t[target+1];
+        memset(t,0,sizeof(t));
+        t[0] = 1;
         for (int i = 1; i <= target; ++i) {
-            for (int x : nums) {
-                if (i >= x) result[i] += result[i - x];
+            for (int j=1; j<=nums.size();j++) {
+                if (i >=nums[j-1]) 
+                    t[i] += t[i - nums[j-1]];
             }
         }
         
-        return result[target];
+        return t[target];
     }
     
 };
+
+// class Solution {
+// public:
+//      int combinationSum4(vector<int>& nums, int target) {
+//         vector<unsigned int> result(target + 1);
+//         result[0] = 1;
+//         for (int i = 1; i <= target; ++i) {
+//             for (int x : nums) {
+//                 if (i >= x) result[i] += result[i - x];
+//             }
+//         }
+        
+//         return result[target];
+//     }
+    
+// };
