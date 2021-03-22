78c6b77a4af6a686b8ea38dab6bbf04018a22a9b
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-22 10:13:04 +0530

    Time: 8 ms (92.47%), Space: 9.6 MB (97.52%) - LeetHub

--- minimum-path-sum/minimum-path-sum.cpp
+++ minimum-path-sum/minimum-path-sum.cpp
@@ -3,29 +3,48 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int t[m+1][n+1];
-        memset(t,0,sizeof(t));
-        int sum=0;
-        for(int i=1;i<=m;i++)
-        {
-            sum+=grid[i-1][0];
-            t[i][1]=sum;
-        }
-        sum=0;
-	    for(int j=1;j<=n;j++)
-	    {
+//         int t[m+1][n+1];
+//         memset(t,0,sizeof(t));
+//         int sum=0;
+//         for(int i=1;i<=m;i++)
+//         {
+//             sum+=grid[i-1][0];
+//             t[i][1]=sum;
+//         }
+//         sum=0;
+// 	    for(int j=1;j<=n;j++)
+// 	    {
             
-            sum+=grid[0][j-1];
-            t[1][j]=sum;
-        }
-        for(int i=2;i<=m;i++)
+//             sum+=grid[0][j-1];
+//             t[1][j]=sum;
+//         }
+//         for(int i=2;i<=m;i++)
+//         {
+//             for(int j=2;j<=n;j++)
+//             {
+//                 t[i][j]=min(t[i-1][j],t[i][j-1])+grid[i-1][j-1];
+//             }
+//         }
+//         return t[m][n];
+        
+        
+            int sum=0;
+        for(int i=1;i<m;i++)
         {
-            for(int j=2;j<=n;j++)
+            grid[i][0]+=grid[i-1][0];
+        }
+        for(int j=1;j<n;j++)
+        {
+            grid[0][j]+=grid[0][j-1];
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
             {
-                t[i][j]=min(t[i-1][j],t[i][j-1])+grid[i-1][j-1];
+                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
             }
         }
-        return t[m][n];
+        return grid[m-1][n-1];
     }
     
 };
