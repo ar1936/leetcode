1a5ce8914c447944e73183781d9bc9bae448480b
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-03-25 11:58:35 +0530

    Time: 4 ms (27.19%), Space: 7.9 MB (8.24%) - LeetHub

--- is-subsequence/is-subsequence.cpp
+++ is-subsequence/is-subsequence.cpp
@@ -1,18 +1,16 @@
-//2 pointer 
-
-
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int s_id = 0, t_id = 0;
-        while(s_id < s.size() && t_id < t.size()) {
-            t_id = t.find(s[s_id], t_id);
-            if (t_id >= t.size()) {
-                return false;
+        int m = s.length() , n=t.length(); 
+        vector<vector<int>> lcs(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+               if(s[i-1]==t[j-1])  
+                    lcs[i][j] = 1+lcs[i-1][j-1];
+                else 
+                    lcs[i][j] = max(lcs[i-1][j],lcs[i][j-1]);
             }
-            t_id++;
-            s_id++;
         }
-        return s_id == s.size();
+        return lcs[m][n] == m;
     }
 };
