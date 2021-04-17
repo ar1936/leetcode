f1e84ddf4a7cd5261ded01fda54bbe426ed2b6ba
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-04-17 07:56:15 +0530

    Time: 4 ms (80.04%), Space: 9.6 MB (46.54%) - LeetHub

--- search-insert-position/search-insert-position.cpp
+++ search-insert-position/search-insert-position.cpp
@@ -14,8 +14,8 @@ public:
         int lo=0,hi=nums.size()-1;
         while(lo <= hi){
             int mid = lo + (hi-lo)/2;
-            if(target == nums[mid]) return mid;
-            else if(target > nums[mid]) lo = mid+1;
+            // if(target == nums[mid]) return mid;
+            if(target > nums[mid]) lo = mid+1;
             else hi = mid-1;
         }
         return lo;
