7c8fd50ae9684273416de9c2ec5685240d02e118
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-04-19 11:26:33 +0530

    Time: 292 ms (5.09%), Space: 110.2 MB (87.51%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.cpp
+++ palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,21 +10,37 @@
  */
 class Solution {
 public:
-    bool solve(vector<int>v){
-        if(v.size()==1)
-            return true;
-        for(int i=0;i<v.size()/2;i++){
-            if(v[i]!=v[v.size()-i-1])
+    // bool solve(vector<int>v){
+    //     if(v.size()==1)
+    //         return true;
+    //     for(int i=0;i<v.size()/2;i++){
+    //         if(v[i]!=v[v.size()-i-1])
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    // bool isPalindrome(ListNode* head) {
+    //     vector<int>v;
+    //     while(head!=NULL){
+    //         v.push_back(head->val);
+    //         head=head->next;
+    //     }
+    //     return solve(v);
+    // }
+    
+
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head, *prev = NULL, *tmp;
+        while (fast && fast->next) // finding mid point
+            fast = fast->next->next,
+            tmp = slow->next, slow->next = prev, prev = slow, slow = tmp;
+        slow = (fast ? slow->next : slow); // for odd length case as mentioned above
+        while (slow)                       // check if linked lists starting at prev and slow are equal
+            if (slow->val != prev->val)
                 return false;
-        }
+            else
+                slow = slow->next, prev = prev->next;
         return true;
-    }
-    bool isPalindrome(ListNode* head) {
-        vector<int>v;
-        while(head!=NULL){
-            v.push_back(head->val);
-            head=head->next;
-        }
-        return solve(v);
     }
 };
