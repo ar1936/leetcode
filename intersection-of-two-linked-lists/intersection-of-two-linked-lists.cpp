eb85f60614dffad8ae7c731f6e5d65526c469f05
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-04-20 08:55:20 +0530

    Time: 44 ms (59.75%), Space: 17.1 MB (14.31%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,28 +1,94 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//     //     unordered_set<ListNode*>s;
+//     //     if(headA==NULL||headB==NULL)
+//     //         return NULL;
+//     //     while(headA!=NULL)
+//     //     {
+//     //       m  s.insert(headA);
+//     //         headA=headA->next;
+//     //     }
+//     //     while(headB!=NULL)
+//     //     {
+//     //         if(s.find(headB)!=s.end())
+//     //             return headB;
+//     //         headB=headB->next;
+//     //     }
+//     //     return NULL;
+//     // }
+        
+        
+        
+//         // ListNode *pA = headA;
+//         // ListNode *pB = headB;
+//         // while (pA != pB) {
+//         //     pA=pA == nullptr ? headB : pA->next;
+//         //     pB=pB == nullptr ? headA : pB->next;
+//         //     // if(pA)
+//         // }
+//         // return pA;
+    
+        
+  
+//     ListNode *p1 = headA;
+//     ListNode *p2 = headB;
+        
+//     if (p1 == NULL || p2 == NULL) return NULL;
+
+//     while (p1 != NULL && p2 != NULL && p1 != p2) {
+//         p1 = p1->next;
+//         p2 = p2->next;
+
+//         //
+//         // Any time they collide or reach end together without colliding 
+//         // then return any one of the pointers.
+//         //
+//         if (p1 == p2) return p1;
+
+//         //
+//         // If one of them reaches the end earlier then reuse it 
+//         // by moving it to the beginning of other list.
+//         // Once both of them go through reassigning, 
+//         // they will be equidistant from the collision point.
+//         //
+//         if (p1 == NULL) p1 = headB;
+//         if (p2 == NULL) p2 = headA;
+//     }
+        
+//     return p1;
+
+//     }
+        
+// };
+
+
+
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        set<ListNode*>s;
-        if(headA==NULL||headB==NULL)
-            return NULL;
-        while(headA!=NULL)
-        {
+        unordered_set<ListNode*>s;
+        while(headA){
             s.insert(headA);
             headA=headA->next;
         }
-        while(headB!=NULL)
-        {
+        while(headB){
             if(s.find(headB)!=s.end())
                 return headB;
             headB=headB->next;
         }
         return NULL;
+        
     }
+        
 };
+
