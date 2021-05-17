c0cb860ed7ea5a9da793d6df8d4700b02203c594
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-05-17 12:29:33 +0530

    Time: 36 ms (99.86%), Space: 21.8 MB (63.32%) - LeetHub

---
+++ number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>&grid,int x,int y,int N,int M){
+        if(x<0||x>=N||y<0||y>=M||grid[x][y]==0)
+            return;
+        grid[x][y]=0;
+        solve(grid,x,y+1,N,M);
+        solve(grid,x-1,y,N,M);
+        solve(grid,x+1,y,N,M);
+        solve(grid,x,y-1,N,M);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int N=grid.size();
+        int M=grid[0].size();
+        int ans=0;
+        for(int i=0;i<N;i++){
+            solve(grid,i,0,N,M);
+            if(M>1){
+                solve(grid,i,M-1,N,M);
+            }
+        }
+        for(int j=1;j<M-1;j++){
+            solve(grid,0,j,N,M);
+            if(N>1){
+                solve(grid,N-1,j,N,M);\
+            }
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                ans+=grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
