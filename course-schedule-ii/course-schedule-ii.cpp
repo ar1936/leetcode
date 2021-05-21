de346dff2cefb90ba625e95634c9c2c93dac6590
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-05-21 17:04:31 +0530

    Time: 32 ms (16.28%), Space: 14.1 MB (54.10%) - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> pre_req_count(numCourses,0);
-        unordered_map<int,vector<int>> before_after;
-        vector<int> res;
+ 
+        vector<int> indegree(numCourses);
+        vector<vector<int>> graph(numCourses);
+        vector<int> ans(numCourses);
         
-        for (int i=0; i<prerequisites.size(); ++i) {
-            // 
-            before_after[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            //increase pre-req count of this course needed by 1 
-            pre_req_count[prerequisites[i][0]]++;
+        for(auto p:prerequisites){
+            graph[p[1]].push_back(p[0]),indegree[p[0]]++;
         }
         
-        int total = 0, curr;
         queue<int> q;
-        //find all courses that don't have any prerequisites and add to queue
-        for (int i=0; i<pre_req_count.size(); ++i) {
-            if (pre_req_count[i] == 0)
-                q.push(i);
+        
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
         }
         
-        while (!q.empty()) {
-            curr = q.front(); q.pop();
-            res.push_back(curr);
-            //find all courses that has this course as pre-req
-            for (auto &i : before_after[curr]) {
-                //this course now has one less pre-req
-                pre_req_count[i]--;
-                //if now this course is pre-req free, add to q
-                if (pre_req_count[i] == 0)
-                    q.push(i);
-            } 
-            ++total;
+        int i=0;
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            ans[i++]=node;
+            
+            for(auto nbr:graph[node]){
+                indegree[nbr]--;
+                
+                if(indegree[nbr]==0){
+                    q.push(nbr);
+                }
+            }
         }
-        if (total != numCourses)
-            return {};
-        return res;
+        if(i==numCourses) return ans;
+        return {};
     }
 };
