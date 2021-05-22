7a4eaf3244a4dd4ec5a1ef3a9d093a176f3378ef
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-05-22 09:31:17 +0530

    Time: 16 ms (93.32%), Space: 13.4 MB (89.66%) - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
  
         vector<int> indegree(numCourses);
-        vector<vector<int>> graph(numCourses);
+        vector<vector<int>> adj(numCourses);
         vector<int> ans(numCourses);
         
-        for(auto p:prerequisites){
-            graph[p[1]].push_back(p[0]),indegree[p[0]]++;
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]),indegree[prerequisites[i][0]]++;
         }
         
         queue<int> q;
@@ -18,15 +18,15 @@ public:
         
         int i=0;
         while(!q.empty()){
-            auto node=q.front();
+            int node=q.front();
             q.pop();
             ans[i++]=node;
             
-            for(auto nbr:graph[node]){
-                indegree[nbr]--;
+            for(int i=0;i<adj[node].size();i++){
+                indegree[adj[node][i]]--;
                 
-                if(indegree[nbr]==0){
-                    q.push(nbr);
+                if(indegree[adj[node][i]]==0){
+                    q.push(adj[node][i]);
                 }
             }
         }
