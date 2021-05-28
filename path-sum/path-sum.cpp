bef418033fd52ad4ce1f9e59489c2fbca6228884
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-05-28 18:01:29 +0530

    Time: 8 ms (86.48%), Space: 21.1 MB (86.11%) - LeetHub

--- path-sum/path-sum.cpp
+++ path-sum/path-sum.cpp
@@ -1,31 +1,25 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 // class Solution {
-//     int solve(TreeNode*root,int sum){
-//         if(root==NULL)
-//             return 0;
-//         sum=sum-root->val;
-//         solve(root->left,sum);
-//         solve(root->right,sum);
-//         return sum;
-//     }
 // public:
-//     bool hasPathSum(TreeNode* root, int targetSum) {
-//         targetSum-=root->val;
-//         if(targetSum==0)
-//             return 0;
-//         int m=solve(root,targetSum);
-//         if(m==0||targetSum==0)
+//     bool hasPathSum(TreeNode* root, int sum) {
+//         if(root==NULL)
+//             return false;
+//         if(sum==0)
 //             return true;
+//         hasPathSum(root->left, sum-=root->val);
+//         sum+=root->val;
+//         hasPathSum(root->right, sum-=root->val);
+//         sum+=root->val;
 //         return false;
 //     }
 // };
@@ -33,11 +27,13 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-        if(!root)
-            return false;                                         //Terminating Condition
-        sum=sum-root->val;                                             //Body
+        if(root==NULL)
+            return false;                                         
+        sum=sum-root->val;                                           
         if(sum==0&&!root->left&&!root->right)
-            return true;              //body
-        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);//Propagation
+            return true; 
+        bool left_flag=hasPathSum(root->left,sum);
+        bool right_flag=hasPathSum(root->right,sum);
+        return left_flag||right_flag;
     }
 };
