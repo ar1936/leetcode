964eb0bce0cd9989a2eb293fbdc9e194781f840f
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-05-30 14:18:14 +0530

    Time: 4 ms (97.76%), Space: 10.4 MB (12.19%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-       return partition(nums,0,nums.size()-1,nums.size()-k+1);//convert to (size-k+1)th smallest problem.
-       
-    }
-
-    int partition(vector<int>&nums,int left,int right,int k){// k denote the kth smallest in the nums
-        int i=left,j=right,x=nums[left];
-        while(i<j){
-            while(i<j&&nums[j]>=x)j--;
-            nums[i]=nums[j];
-            while(i<j&&nums[i]<x)i++;
-            nums[j]=nums[i];
+        priority_queue<int>pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>(nums.size()-k+1)){
+                pq.pop();
+            }
         }
-        nums[i]=x;
-        if(i==k-1) return nums[i];
-        else if(i>k-1) return partition(nums,left,i-1,k);
-        else return partition(nums,i+1,right,k);
+        return pq.top();
     }
 };
