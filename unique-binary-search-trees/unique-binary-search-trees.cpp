b5a90a59dec24de23ae1029ab4d88d29fafd29cb
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-06-08 08:24:32 +0530

    Time: 4 ms (10.04%), Space: 5.9 MB (82.76%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,25 @@
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         vector<int>dp(n+1);
+//         dp[0]=1;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 dp[i]+=dp[j-1]*dp[i-j];
+//             }
+//         }
+//         return dp[n];
+//     }
+// };
+
+class Solution {
+public:
+    int numTrees(int n) {
+        long long C=1;
+        for(int i=0;i<n;i++){
+            C=C*2*(2*i+1)/(i+2);
+        }
+        return (int)C;
+    }
+};
