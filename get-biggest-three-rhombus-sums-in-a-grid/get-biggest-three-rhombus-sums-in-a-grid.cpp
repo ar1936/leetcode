5cdea56ea19b030f3bb393aeb381d5d065f0c9ca
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-06-18 17:19:15 +0530

    Time: 220 ms (77.51%), Space: 61.9 MB (23.15%) - LeetHub

---
+++ get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+    set<int> s;
+    int m = size(grid), n = size(grid[0]), MAX_SIZE = (min(m, n) >> 1) + 1;
+    // single cells
+    for(auto row : grid) 
+        for(auto el : row) {
+            s.insert(el);
+            if(size(s) > 3) s.erase(begin(s));
+        }
+
+    for(int Len = 1; Len < MAX_SIZE; Len++) 
+        for(int row = Len; row < m - Len; row++) 
+            for(int col = Len; col < n - Len; col++) {                     
+                int i = row, j = col - Len, sum = 0;
+                // left to top diagonal
+                while(j <= col)  
+                    sum += grid[i][j], i--, j++;
+                i = row, j = col + Len;
+                // right to bottom diagonal
+                while(j >= col)
+                    sum += grid[i][j],  i++, j--;
+                i = row - Len + 1, j = col + 1;
+                // top to right diagonal
+                while(i < row)   
+                    sum += grid[i][j],  i++, j++;
+                i = row + Len - 1, j = col - 1;
+                // bottom to left diagonal
+                while(i > row)
+                    sum += grid[i][j],  i--, j--;
+                s.insert(sum);
+                if(size(s) > 3) s.erase(begin(s));
+            }
+    return vector<int>(rbegin(s), rend(s));
+}
+};
