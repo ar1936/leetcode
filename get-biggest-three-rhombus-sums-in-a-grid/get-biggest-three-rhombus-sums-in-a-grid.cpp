a973232045a6374c03bb280f098dda32ffa9ff73
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-06-18 17:48:35 +0530

    Time: 232 ms (75.01%), Space: 62 MB (23.07%) - LeetHub

--- get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
     set<int> s;
-    int m = size(grid), n = size(grid[0]), MAX_SIZE = (min(m, n) >> 1) + 1;
+    int m = size(grid), n = size(grid[0]), MAX_SIZE = (min(m, n) /2) + 1;
     // single cells
     for(auto row : grid) 
-        for(auto el : row) {
-            s.insert(el);
-            if(size(s) > 3) s.erase(begin(s));
+        for(auto element : row) {
+            s.insert(element);
+            if(s.size() > 3) 
+                s.erase(s.begin());
         }
 
     for(int Len = 1; Len < MAX_SIZE; Len++) 
@@ -15,22 +16,39 @@ public:
             for(int col = Len; col < n - Len; col++) {                     
                 int i = row, j = col - Len, sum = 0;
                 // left to top diagonal
-                while(j <= col)  
-                    sum += grid[i][j], i--, j++;
+                while(j <= col)  {
+                    sum += grid[i][j];
+                    i--;
+                    j++;
+                }
                 i = row, j = col + Len;
                 // right to bottom diagonal
-                while(j >= col)
-                    sum += grid[i][j],  i++, j--;
+                
+                while(j >= col){
+                    sum += grid[i][j];
+                    i++;
+                    j--;
+                }
                 i = row - Len + 1, j = col + 1;
                 // top to right diagonal
-                while(i < row)   
-                    sum += grid[i][j],  i++, j++;
+                while(i < row)  { 
+                    sum += grid[i][j];
+                    i++;
+                    j++;
+                }
+                
                 i = row + Len - 1, j = col - 1;
                 // bottom to left diagonal
-                while(i > row)
-                    sum += grid[i][j],  i--, j--;
+                
+                while(i > row){
+                    sum += grid[i][j];
+                    i--;
+                    j--;
+                }
+                
                 s.insert(sum);
-                if(size(s) > 3) s.erase(begin(s));
+                if(size(s) > 3) 
+                    s.erase(s.begin());
             }
     return vector<int>(rbegin(s), rend(s));
 }
