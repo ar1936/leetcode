ff2e3d7c3aee26553d6132bffad92e933bd17f44
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-06-29 08:26:49 +0530

    Time: 4 ms (74.42%), Space: 11.3 MB (5.37%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,33 +1,41 @@
 class Solution {
-public:
+    int b_s2(vector<int>nums,int ind,int target){
+        int lo=ind,hi=nums.size()-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                hi=mid-1;
+            else
+                lo=mid+1;
+        }
+        return -1;
+    }
+    int b_s1(vector<int>nums,int ind, int target){
+        int lo=0,hi=ind;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                hi=mid-1;
+            else
+                lo=mid+1;
+        }
+        return -1;
+    }
+    public:
     int search(vector<int>& nums, int target) {
-        // support variables
-        int len = nums.size(), l = 0, r = len, mid;
-        // finding the pivot
-        while (l < r) {
-            mid = (l + r) / 2;
-            // current element > first elemen: we move the window right
-            if (nums[mid] >= nums[0]) l = mid + 1;
-            // otherwise we move it left
-            else r = mid;
+        int lo=0,hi=nums.size()-1;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]>nums[hi])
+                lo=mid+1;
+            else
+                hi=mid;
         }
-        // checking for edge case - pivot at the end = sorted vector
-        if (l == len) l = 0;
-        // preparing for the next BS: target is between pivot and the last element
-        if (target >= nums[l] && target <= nums.back()) r = len;
-        // target is between the first element and the one before pivot
-        else r = l, l = 0;
-        // all other cases
-        // finding the element
-        while (l < r) {
-            mid = (l + r) / 2;
-            // mid matches the target element: we return it
-            if (nums[mid] == target) return mid;
-            // mid matches an element smaller than target: we move the window right
-            else if (nums[mid] < target) l = mid + 1;
-            // otherwise we move it left
-            else r = mid;
-        }
-        return nums[l] == target ? l : -1;
+        return b_s1(nums,hi-1,target)==-1?b_s2(nums,hi,target):b_s1(nums,hi-1,target);
+        // return hi;
     }
 };
