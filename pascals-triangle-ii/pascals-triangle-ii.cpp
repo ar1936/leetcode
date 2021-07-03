6972616986d2b9787906cc12d4c8f2047cb9398c
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-03 05:56:11 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (30.65%) - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+      vector<vector<int>>v (n+1);
+        if(n==0)
+            return {1};
+        for(int i=0;i<=n;i++)
+        {
+            v[i].resize(i + 1); 
+            v[i][0]=1;
+            v[i][i]=1;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+                v[i][j]=v[i-1][j-1]+v[i-1][j];
+            }
+        }
+        return v[n];
+    }
+};
