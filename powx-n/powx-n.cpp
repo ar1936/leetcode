2b6f135f4c95961871a280a9188f8b1db3a2deb5
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-05 17:34:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.47%) - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long long i = N; i ; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
     }
 };
