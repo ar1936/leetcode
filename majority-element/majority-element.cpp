d9130085961ede27cb63658fddb19f19e3ceb665
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-05 17:38:58 +0530

    Time: 20 ms (53.70%), Space: 19.6 MB (47.66%) - LeetHub

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-int majorityElement(vector<int>& nums) {
-    int candidate = nums[0], counter = 0;
-    for (int n: nums) {
-        if (counter == 0) {
-            candidate = n;
-            counter = 1;
-        } else if (candidate == n) {
-            counter++;
-        } else {
-            counter--;
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size(), candidate, counter;
+        srand(unsigned(time(NULL)));
+        while (true) {
+            candidate = nums[rand() % n], counter = 0;
+            for (int num : nums) {
+                if (num == candidate) {
+                    counter++;
+                }
+            }
+            if (counter > n / 2) {
+                break;
+            }
         }
+        return candidate;
     }
-    return candidate;
-}
 };
-
-
-
-
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         map<int,int>mp;
-//         int m=0;
-//         if(nums.size()%2==0)
-//             m=nums.size()/2;
-//         else
-//             m=(nums.size()+1)/2;
-        
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             mp[nums[i]]++;
-//             if(mp[nums[i]]>=m) 
-//                 return nums[i];
-//         }
-//         return 0;
-        
-        
-//     }
-// };
