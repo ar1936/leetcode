304c36c5c38948e8836bc6a9f7b1aa42c4da75bb
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-06 16:08:16 +0530

    Time: 152 ms (24.66%), Space: 72 MB (23.82%) - LeetHub

--- sort-an-array/sort-an-array.cpp
+++ sort-an-array/sort-an-array.cpp
@@ -1,8 +1,33 @@
 class Solution {
+
+    
+    void merge(vector<int>& nums, int l, int m, int r){
+        vector<int> tmp(r - l + 1);
+        int i = l; // index for left subarray
+        int j = m + 1; // index for right subarray
+        int k = 0; // index for temporary array
+        while(i <= m && j <= r){
+            if(nums[i] <= nums[j]) tmp[k++] = nums[i++]; 
+            else tmp[k++] = nums[j++];
+        }
+        while(i <= m) tmp[k++] = nums[i++];
+        while(j <= r) tmp[k++] = nums[j++]; 
+        for(i = 0; i < k; i++) nums[l + i] = tmp[i];
+    }
+	
+    void merge_sort(vector<int>&v,int l,int r){
+        if(l<r){
+        int mid=l+(r-l)/2;
+        merge_sort(v,l,mid);
+        merge_sort(v,mid+1,r);
+        merge(v,l,mid,r);
+        }
+    }
+    
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        merge_sort(nums,0,nums.size()-1);
         return nums;
-        
     }
 };
+
