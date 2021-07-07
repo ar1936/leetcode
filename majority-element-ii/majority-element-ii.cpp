0c95383051da3661c651d04a65523a65e2c620cd
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-07 14:50:09 +0530

    Time: 12 ms (74.89%), Space: 15.8 MB (46.03%) - LeetHub

--- majority-element-ii/majority-element-ii.cpp
+++ majority-element-ii/majority-element-ii.cpp
@@ -1,46 +1,70 @@
-// class Solution {
-//    int randRange(Random rand, int min, int max) {
-//         return rand.nextInt(max - min) + min;
-//     }
-
-//      int countOccurences(int nums[], int num) {
-//         int count = 0;
-//         for (int i = 0; i < nums.length; i++) {
-//             if (nums[i] == num) {
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-
-//     public int majorityElement(int[] nums) {
-//         Random rand = new Random();
-
-//         int majorityCount = nums.length/3;
-//         vector<int>ans;
-//         while (true) {
-//             int candidate = nums[randRange(rand, 0, nums.length)];
-//             if (countOccurences(nums, candidate) > majorityCount) {
-//                 ans.push_back(candidate);
-//             }
-//         }
-//         return ans;
-//     }
-// }
-
-
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-		if (nums.empty()) return ans;
-		// srand(time(NULL));
-        for (int i = 0; i < 100; ++i) {
-            int val = nums[rand() % nums.size()], cnt = 0;
-            for (int j = 0; j < ans.size(); ++j) if (ans[j] == val) goto outer;
-            for (int j = 0; j < nums.size(); ++j) if (val == nums[j]) ++cnt;
-            if (cnt > nums.size() / 3) ans.push_back(val);   
-outer:; }     
+        // support variables
+        int cand1, vote1 = 0, cand2, vote2 = 0, lmt = nums.size() / 3;
+        // looking for the 2 most frequent items
+        for (int n: nums) {
+            if (n == cand1) vote1++;
+            else if (n == cand2) vote2++;
+            else if (!vote1) {
+                cand1 = n;
+                vote1++;
+            }
+            else if (!vote2) {
+                cand2 = n;
+                vote2++;
+            }
+            else {
+                vote1--;
+                vote2--;
+            }
+        }
+        // checking the actual frequency of the 2 most common items
+        vote1 = 0;
+        vote2 = 0;
+        for (int n: nums) {
+            if (n == cand1) vote1++;
+            if (n == cand2) vote2++;
+        }
+        // making sure cand1 has more votes
+        if (vote1 < vote2) {
+            swap(cand1, cand2);
+            swap(vote1, vote2);
+        }
+        vector<int>ans;
+        if(vote1>lmt)
+            ans.push_back(cand1);
+        if(vote2>lmt)
+            ans.push_back(cand2);
         return ans;
+        
+        // return vote1 <= lmt ? vector<int>{} : vote2 <=lmt ? vector<int>{cand1} : vector<int>{cand1, cand2};
+        return {};
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int len = nums.size();
+//         vector<int> v;
+//         if(len == 0) return v;
+        
+//         for(int i = 0; i < 10; i++){
+//             int c = 0;
+//             int x = nums[rand() % len];
+//             for(int j = 0; j < len; j++)
+//                 if(x == nums[j])
+//                     c++;
+            
+//             if(c > len / 3){
+//                 if (find(v.begin(), v.end(), x) == v.end())
+//                     v.push_back(x);
+//             }
+//         }
+        
+//         return v;
+//     }
+// };
