aa7518f2d5b7a5e43d3b6d10beb5782393328a17
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-08 18:11:39 +0530

    Time: 312 ms (30.61%), Space: 27.7 MB (12.24%) - LeetHub

--- 4sum-ii/4sum-ii.cpp
+++ 4sum-ii/4sum-ii.cpp
@@ -1,23 +1,37 @@
+// class Solution {
+// public:
+//     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+//         int ans=0,n=nums1.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 for(int k=0;k<n;k++){
+//                     for(int l=0;l<n;l++){
+//                         if(nums1[i]+nums2[j]+nums3[k]+nums4[l]==0)
+//                             ans++;
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        map<int,int>mp;
-        for(int i=0;i<A.size();i++)
-        {
-            for(int j=0;j<B.size();j++)
-            {
-                mp[A[i]+B[j]]++;
-            }
+    int fourSumCount(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d) {
+        // support variables
+        int res = 0;
+        unordered_map<int, int> abMap, cdMap;
+        // populating abMap and cdMap as frequency tables of sums
+        for (int i: a) for (int j: b) abMap[i + j]++;
+        for (int i: c) for (int j: d) cdMap[i + j]++;
+        // making sure we loop through the smaller of the 2
+        if (abMap.size() > cdMap.size()) swap(abMap, cdMap);
+        // finding if we have opposite matches in abMap and cdMap
+        for (auto e: abMap) {
+            // otherwise we update res
+            res += e.second * cdMap[-e.first];
         }
-        int count=0;
-        for(int i=0;i<C.size();i++)
-        {
-            for(int j=0;j<D.size();j++)
-            {
-                if(mp.find(-C[i]-D[j])!=mp.end())
-                    count+=mp[-C[i]-D[j]];
-            }
-        }
-        return count;
+        return res;
     }
 };
