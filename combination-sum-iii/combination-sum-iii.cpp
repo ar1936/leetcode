88213d2f6025d20d9ea4e664d8fcca6d24ac4628
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-07-15 15:03:18 +0530

    Time: 4 ms (35.33%), Space: 8.6 MB (10.76%) - LeetHub

--- combination-sum-iii/combination-sum-iii.cpp
+++ combination-sum-iii/combination-sum-iii.cpp
@@ -1,54 +1,33 @@
-// class Solution {
-// public:
-//     vector<vector<int>> combinationSum3(int k, int m) {
-//         vector<int>nums(k);
-//         for(int i=0;i<k;i++){
-//             nums[i]=(i+1);
-//         }
-        
-//         int n=k, p=1<<n;
-//         vector<vector<int>>temp(p),ans;
-        
-//         for(int i=0;i<p;i++){
-//             for(int j=0;j<k;j++){
-//                 temp[i].push_back(nums[j]);
-//             }
-//         }
-//         int sum=0;
-        
-//         for(int i=0;i<p;i++){
-//             sum=0;
-//             if(temp[i].size()==k){
-//                 for(auto x:temp[i]){
-//                     sum+=x;
-//                 }
-//             }
-//             if(sum==m){
-//                 ans.push_back(temp[i]);
-//             }
-//         }
-//         return temp;
-//     }
-// };
-
 class Solution {
-public:
-  void combination(vector<vector<int>>& result, vector<int> sol, int k, int n) {
-    if (sol.size() == k && n == 0) { result.push_back(sol); return ; }
-    if (sol.size() < k) {
-      for (int i = sol.empty() ? 1 : sol.back() + 1; i <= 9; ++i) {
-        if (n - i < 0) break;
-        sol.push_back(i);
-        combination(result, sol, k, n - i);
-        sol.pop_back();
-      }
+    vector<vector<int>>nums,ans;
+    void solve(vector<int>&temp,int n, int k, int i){
+        if(temp.size()==k){
+            nums.push_back(temp);
+            return ;
+        }
+        if(i==n+1){
+            return ;
+        }
+        temp.push_back(i);
+        solve(temp,n,k,i+1);
+        temp.pop_back();
+        solve(temp,n,k,i+1);
     }
-  }
-
-  vector<vector<int>> combinationSum3(int k, int n) {
-    vector<vector<int>> result;
-    vector<int> sol;
-    combination(result, sol, k, n);
-    return result;
-  }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>temp;
+        solve(temp,9,k,1);
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum=0;
+            for(auto x:nums[i]){
+                sum+=x;
+            }
+            if(sum==n){
+                
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
 };
