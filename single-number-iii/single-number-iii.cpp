5f53cd036bd140dfe2a7d967e0d44b009812da5c
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-08-12 01:54:28 +0530

    Time: 16 ms (12.42%), Space: 11.1 MB (6.64%) - LeetHub

---
+++ single-number-iii/single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto x:nums){
+            mp[x]++;
+        }
+        vector<int>ans;
+        for(auto x:mp){
+            if(x.second==1){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
