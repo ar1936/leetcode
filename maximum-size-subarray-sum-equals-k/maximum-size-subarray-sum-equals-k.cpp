bfca05412ccdb7f1e8ac67ca18a31d3782a18af5
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2021-08-17 10:20:45 +0530

    Time: 164 ms (42.93%), Space: 52 MB (90.81%) - LeetHub

---
+++ maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        unordered_map<int, int> sums;
+        int cur_sum = 0;
+        int max_len = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            cur_sum += nums[i];
+            if (cur_sum == k) {
+                max_len = i + 1;
+            } else if (sums.find(cur_sum - k) != sums.end()) {
+                max_len = max(max_len, i - sums[cur_sum - k]);
+            }
+            if (sums.find(cur_sum) == sums.end()) {
+                sums[cur_sum] = i;
+            }            
+        }
+        return max_len;
+    }
+};
