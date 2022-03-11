cece4286ab7b563b574c53b8dfd59f2469e323bb
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2022-03-11 10:41:47 +0530

    Time: 19 ms (13.83%), Space: 11.8 MB (6.63%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -8,31 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+                // using vector 
+// class Solution {
+// public:
+//     ListNode* rotateRight1(ListNode* head, int k) {
+//         if(head==NULL)
+//             return NULL;
+//         vector<int>v;
+//         while(head!=NULL){
+//             v.push_back(head->val);
+//             head=head->next;
+//         }
+//         k=k%v.size();
+//         vector<int>v1(v.size());
+//         for(int i=k;i<v.size();i++){
+//             v1[i]=v[i-k];
+//         }
+//         for(int i=0;i<k;i++){
+//             v1[i]=v[v.size()-k+i];
+//         }
+//         ListNode *ans=new ListNode (v1[0]);
+//         ListNode *temp=ans;
+//         for(int i=1;i<v1.size();i++){
+//             ListNode *node = new ListNode(v1[i]);
+//             temp->next=node;
+//             temp=temp->next;
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL)
-            return NULL;
-        vector<int>v;
-        while(head!=NULL){
-            v.push_back(head->val);
-            head=head->next;
+        if(!head) return head;
+        
+        int len=1; // number of nodes
+        ListNode *newH, *tail;
+        newH=tail=head;
+        
+        while(tail->next)  // get the number of nodes in the list
+        {
+            tail = tail->next;
+            len++;
         }
-        k=k%v.size();
-        vector<int>v1(v.size());
-        for(int i=k;i<v.size();i++){
-            v1[i]=v[i-k];
+        tail->next = head; // circle the link
+
+        if(k %= len) 
+        {
+            for(auto i=0; i<len-k; i++) 
+                tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
         }
-        for(int i=0;i<k;i++){
-            v1[i]=v[v.size()-k+i];
-        }
-        ListNode *ans=new ListNode (v1[0]);
-        ListNode *temp=ans;
-        for(int i=1;i<v1.size();i++){
-            ListNode *node = new ListNode(v1[i]);
-            temp->next=node;
-            temp=temp->next;
-        }
-        return ans;
+        newH = tail->next; 
+        tail->next = NULL;
+        return newH;
     }
 };
