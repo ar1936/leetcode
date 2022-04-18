a9597b5cf9253e033226178686c0c7f986e52f6b
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2022-04-18 14:03:05 +0530

    Time: 8 ms (29.23%), Space: 6 MB (26.51%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        for(long long i=0;i<1e5;i++){
+        for(long long i=0;i*i<=c;i++){
             if(floor(sqrt(c-i*i))==ceil(sqrt(c-i*i)))
                 return true;
         }
