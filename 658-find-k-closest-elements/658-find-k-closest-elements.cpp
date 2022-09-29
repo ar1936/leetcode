3017ba338d7f0bdbe3159675da1f83c6ce65f9a5
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2022-09-29 08:58:46 +0530

    Time: 93 ms (60.22%), Space: 31.2 MB (86.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -2,19 +2,45 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         // priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        priority_queue<pair<int,int>>pq;
-        for(int i=0;i<arr.size();i++){
-            pq.push({abs(arr[i]-x),arr[i]});
-            if(pq.size()>k){
-                pq.pop();
+        // priority_queue<pair<int,int>>pq;
+        // for(int i=0;i<arr.size();i++){
+        //     pq.push({abs(arr[i]-x),arr[i]});
+        //     if(pq.size()>k){
+        //         pq.pop();
+        //     }
+        // }
+        // vector<int>ans;
+        // while(pq.empty()==false){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
+        
+        
+        
+        
+        int n = arr.size();
+        
+        if(k==n){
+            return arr;
+        }
+        if(x<=arr[0]){
+            return vector<int>(arr.begin()+0, arr.begin()+k);
+        }
+        else if(x>=arr[n-1]){
+            return vector<int>(arr.begin()+n-k,arr.end());
+        }
+        else{
+            int lo=0,hi=n-1,p=n-k;
+            while(p--){
+                if(abs(x-arr[lo])>abs(arr[hi]-x))
+                    lo++;
+                else
+                    hi--;
             }
+            hi++;
+            return vector<int>(arr.begin()+lo, arr.begin()+hi);
         }
-        vector<int>ans;
-        while(pq.empty()==false){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
     }
 };
