3745f9c21d40aeb94ba6aabb045ec8ddc0c9dcb5
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2022-10-02 11:26:16 +0530

    Time: 52 ms (67.50%), Space: 71.5 MB (51.48%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -9,46 +9,36 @@
  * };
  */
 class Solution {
+    void solve(ListNode* l1, ListNode* l2, ListNode* tmp, int rem){
+        if(l1==NULL && l2==NULL){
+            if(rem!=0){
+                ListNode *node = new ListNode(rem);
+                tmp ->next = node;
+                tmp = tmp ->next;
+            }
+            return ;
+        }
+        int sum=0,x=0,y=0;
+        if(l1!=NULL){
+            x=l1->val;
+        }
+        if(l2!=NULL){
+            y=l2->val;
+        }
+        sum=x+y+rem;
+        rem=sum/10;
+        sum=sum%10;
+        ListNode *node = new ListNode(sum);
+        tmp ->next = node;
+        tmp = tmp ->next;
+        solve(l1==NULL?l1:l1->next,l2==NULL?l2:l2->next,tmp,rem);
+    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode * ans = new ListNode();
+        ListNode * ans = new ListNode(0);
         ListNode * tmp = ans;
-        int sum=0,rem=0;
-        while(l1&&l2){
-            sum=l1->val + l2->val + rem;
-            rem = sum/10;
-            sum=sum%10;
-            ListNode * node = new ListNode(sum);
-            tmp -> next = node;
-            tmp = tmp -> next;
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        while(l1){
-            sum=l1->val + rem;
-            rem = sum/10;
-            sum=sum%10;
-            ListNode * node = new ListNode(sum);
-            tmp -> next = node;
-            tmp = tmp -> next;
-            l1 = l1->next;
-        }
+        solve(l1,l2,tmp,0);
         
-        
-        while(l2){
-            sum=l2->val + rem;
-            rem = sum/10;
-            sum=sum%10;
-            ListNode * node = new ListNode(sum);
-            tmp -> next = node;
-            tmp = tmp -> next;
-            l2 = l2->next;
-        }
-        if(rem){
-            ListNode * node = new ListNode(rem);
-            tmp -> next = node;
-            tmp = tmp -> next;
-        }
         return ans->next;
     }
 };
