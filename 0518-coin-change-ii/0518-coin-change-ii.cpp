cb6d9630ea160137b5c24989e4a4b1e40031b4dd
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2022-10-18 09:39:17 +0530

    Time: 35 ms (58.24%), Space: 13.5 MB (62.44%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,31 +1,45 @@
 class Solution {
-    int dp[301][5001];
-    int get_ans(int n, int amount, vector<int>&v){
-      if(n==0||amount<=0){
-        if(amount==0){
-          return 1;
+int dp[301][5001];
+ int get_ans(int n, int amount, vector < int > & v) {
+   if (n == 0 || amount <= 0) {
+     if (amount == 0) {
+       return 1;
+     }
+     return 0;
+   }
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=amount;j++){
+            if(i==0){
+                dp[i][j]=0;
+            }
+            if(j==0)
+            {
+                dp[i][j]=1;
+            }
         }
-        return 0;
-      }
-      if(dp[n][amount]!=-1)
-        return dp[n][amount];
-      if(amount>=v[n-1]){
-         return dp[n][amount]=get_ans(n,amount-v[n-1],v) + get_ans(n-1,amount,v); 
-      }
-      else{
-         return dp[n][amount]=get_ans(n-1,amount,v);
-      }
-
-     return dp[n][amount];
     }
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=amount;j++){
+            if(j>=v[i-1]){
+                dp[i][j]=dp[i][j-v[i-1]]+dp[i-1][j];
+            }    
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+     
+   return dp[n][amount];
+ }
 
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
+        
         for(int i=0;i<=n;i++){
-          for(int j=0;j<=amount;j++){
-            dp[i][j]=-1;
-          }
+            for(int j=0;j<=amount;j++){
+                dp[i][j]=-1;
+            }
         }
         return get_ans(n,amount,coins);   
     }
