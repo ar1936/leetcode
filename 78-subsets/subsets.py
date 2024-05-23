20016d33ffc9bca5cc75adb3cae085022b360c50
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-05-24 00:47:56 +0530

    Time: 32 ms (84.73%) | Memory: 16.6 MB (78.75%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -12,3 +12,93 @@ class Solution:
         ans = []
         solve(0,ans,[])
         return ans
+
+# solve(0, ans, [])
+
+# |
+
+# +-- Include 1: solve(1, ans, [1])
+
+# |   |
+
+# |   +-- Include 2: solve(2, ans, [1, 2])
+
+# |   |   |
+
+# |   |   +-- Include 3: solve(3, ans, [1, 2, 3])
+
+# |   |   |   |
+
+# |   |   |   +-- ans.append([1, 2, 3]) (base case reached, add to ans)
+
+# |   |   |
+
+# |   |   +-- Exclude 3: solve(3, ans, [1, 2])
+
+# |   |       |
+
+# |   |       +-- ans.append([1, 2]) (base case reached, add to ans)
+
+# |   |
+
+# |   +-- Exclude 2: solve(2, ans, [1])
+
+# |       |
+
+# |       +-- Include 3: solve(3, ans, [1, 3])
+
+# |       |   |
+
+# |       |   +-- ans.append([1, 3]) (base case reached, add to ans)
+
+# |       |
+
+# |       +-- Exclude 3: solve(3, ans, [1])
+
+# |           |
+
+# |           +-- ans.append([1]) (base case reached, add to ans)
+
+# |
+
+# +-- Exclude 1: solve(1, ans, [])
+
+#     |
+
+#     +-- Include 2: solve(2, ans, [2])
+
+#     |   |
+
+#     |   +-- Include 3: solve(3, ans, [2, 3])
+
+#     |   |   |
+
+#     |   |   +-- ans.append([2, 3]) (base case reached, add to ans)
+
+#     |   |
+
+#     |   +-- Exclude 3: solve(3, ans, [2])
+
+#     |       |
+
+#     |       +-- ans.append([2]) (base case reached, add to ans)
+
+#     |
+
+#     +-- Exclude 2: solve(2, ans, [])
+
+#         |
+
+#         +-- Include 3: solve(3, ans, [3])
+
+#         |   |
+
+#         |   +-- ans.append([3]) (base case reached, add to ans)
+
+#         |
+
+#         +-- Exclude 3: solve(3, ans, [])
+
+#             |
+
+#             +-- ans.append([]) (base case reached, add to ans)
