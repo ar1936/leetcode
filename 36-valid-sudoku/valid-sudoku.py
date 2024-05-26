8de462795d7abfc2bb7ad8953da83c0fd4b3c8fb
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-05-26 16:47:16 +0530

    Time: 105 ms (6.82%) | Memory: 16.8 MB (7.22%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,52 @@
+# k = (x // 3) * 3 + y // 3
+#             i_lower = (k // 3) * 3
+#             i_upper = i_lower + 3 
+
+#             j_lower = (k % 3) * 3
+#             j_upper = j_lower + 3  
+            
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def row_check(x,y):
+            row = set()
+            for i in range(9):
+                if board[i][j]!='.' and board[i][y] in row:
+                    print(1,i,y)
+                    return False
+                row.add(board[i][y])
+            return True
+
+        def col_check(x,y):
+            col = set()
+            for j in range(9):
+                if board[i][j]!='.' and board[x][j] in col:
+                    print(2,x,j)
+                    return False
+                col.add(board[x][j])
+            return True
+
+        def small_mat_check(x,y):
+            small_mat = set()
+            k = (x // 3) * 3 + y // 3
+            i_lower = (k // 3) * 3
+            i_upper = i_lower + 3 
+
+            j_lower = (k % 3) * 3
+            j_upper = j_lower + 3  
+            
+            for i in range(i_lower, i_upper):
+                for j in range(j_lower, j_upper):
+                    if((i//3 + j//3) == (x//3 + y//3)):
+                        if board[i][j]!='.' and  board[i][j] in small_mat:
+                            print(3,i,j)
+                            return False
+                        small_mat.add(board[i][j])
+            return True
+        
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!= '.' and (row_check(i,j) == False or col_check(i,j)==False or small_mat_check(i,j)==False):
+                    print(i,j)
+                    return False
+        return True
+                    
