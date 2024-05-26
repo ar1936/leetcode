0737ccc48cc21e594d07e8f9c606f8ee2e8f2c1c
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-05-26 13:44:01 +0530

    Time: 76 ms (18.10%) | Memory: 16.9 MB (5.77%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,40 +1,43 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         existing_queen_ind = []
-        def column_check(x, y):
-            for i, j in existing_queen_ind:
-                if j == y:
-                    return False
-            return True
-
-        def diagonal_check(x, y):
-            gap = x - y
-            sum = x + y
-            for i, j in existing_queen_ind:
-                if i - j == gap or i + j == sum:
-                    return False
-            return True
-
-
-        def can_place_queen(x, y):
-            return column_check(x, y) and diagonal_check(x, y)
-
-        def create_board():
-            board = [['.' for _ in range(n)] for _ in range(n)]
-            for x, y in existing_queen_ind:
-                board[x][y] = 'Q'
-            return ["".join(row) for row in board]
-        
-        def backtrack(row):
-            if row == n:
-                ans.append(create_board())
-                return
-            for col in range(n):
-                if can_place_queen(row, col):
-                    existing_queen_ind.append((row, col))
-                    backtrack(row + 1)
-                    existing_queen_ind.pop()
         ans = []
-        backtrack(0)
+        def check_col(x,y):
+            for (i,j) in existing_queen_ind:
+                if y == j:
+                    return False
+            return True
+        
+        def check_dia(x,y):
+            for (i,j) in existing_queen_ind:
+                if (x+y == i+j) or (i-j == x-y):
+                    return False
+            return True
+        def check_valid(x,y):
+            return check_col(x,y) and check_dia(x,y)
+        
+        def make_board():
+            board = []
+            for i in range(n):
+                st = ""
+                for j in range(n):
+                    if (i,j) in existing_queen_ind:
+                        st+='Q'
+                    else:
+                        st+="."
+                board.append(st)
+            return board
 
+        def backtracking(row):
+            if row == n:
+                ans.append(make_board())
+                return
+            
+            for col in range(n):
+                if check_valid(row,col):
+                    existing_queen_ind.append((row,col))
+                    backtracking(row+1)
+                    existing_queen_ind.pop()
+            
+        backtracking(0)
         return len(ans)
