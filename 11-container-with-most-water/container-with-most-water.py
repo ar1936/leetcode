aa6a4c8d5c73d915f5a8cf3a67f31bcb460056e9
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-06-06 23:20:02 +0530

    Time: 650 ms (5.08%) | Memory: 28.7 MB (98.41%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxArea(self, nums: List[int]) -> int:
+        ans = 0 
+        n = len(nums)
+        i = 0
+        j = n-1
+        while(i<n and j>=0):
+            ans = max(ans,(j-i)*min(nums[i],nums[j]))
+            if nums[i]<nums[j]:
+                i+=1
+            else:
+                j-=1
+        
+        ans = max(ans,(j-i)*min(nums[i],nums[j]))
+        nums = nums[::-1]
+        i = 0
+        j = n-1
+        while(i<n and j>=0):
+            ans = max(ans,(j-i)*min(nums[i],nums[j]))
+            if nums[i]<nums[j]:
+                i+=1
+            else:
+                j-=1
+        ans = max(ans,(j-i)*min(nums[i],nums[j]))
+        return ans
