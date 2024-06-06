ccc7f20e873ce71d1866eaeb6631a6e1d583ef55
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-06-06 23:20:24 +0530

    Time: 544 ms (12.51%) | Memory: 29.5 MB (59.95%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -12,14 +12,14 @@ class Solution:
                 j-=1
         
         ans = max(ans,(j-i)*min(nums[i],nums[j]))
-        nums = nums[::-1]
-        i = 0
-        j = n-1
-        while(i<n and j>=0):
-            ans = max(ans,(j-i)*min(nums[i],nums[j]))
-            if nums[i]<nums[j]:
-                i+=1
-            else:
-                j-=1
-        ans = max(ans,(j-i)*min(nums[i],nums[j]))
+        # nums = nums[::-1]
+        # i = 0
+        # j = n-1
+        # while(i<n and j>=0):
+        #     ans = max(ans,(j-i)*min(nums[i],nums[j]))
+        #     if nums[i]<nums[j]:
+        #         i+=1
+        #     else:
+        #         j-=1
+        # ans = max(ans,(j-i)*min(nums[i],nums[j]))
         return ans
