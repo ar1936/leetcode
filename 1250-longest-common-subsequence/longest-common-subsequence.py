ecd18e64a373c3b77318ae554c031f8c760f931c
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-06-08 14:23:51 +0530

    Time: 713 ms (28.06%) | Memory: 40.6 MB (83.92%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,26 +1,18 @@
 class Solution:
-    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        n = len(s1)
-        m = len(s2)
-        dp = [[-1]*(m+1) for _ in range(n+1)]
-        # if s1[0] == s2[0]:
-        #     dp[1][1] = 1
-        # for i in range(1,n+1):
-        #     for j in range(1,m+1):
-        #         if s1[i-1]==s2[j-1]:
-        #             dp[i][j] = dp[i-1][j-1] + 1
-        #         else:
-        #             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        # return dp[n][m]
-        
-        def solve(i, j):
-            if i == n or j == m :
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[-1]*len(text2) for _ in range(len(text1))]
+        # for i in range(len(text1)+1):
+        #     dp[i][0] = 0
+        # for j in range(len(text2)+1):
+        #     dp[0][j] = 0
+        def solve(n,m):
+            if n == len(text1) or m == len(text2):
                 return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s1[i] == s2[j]:
-                dp[i][j] = solve(i+1,j+1)+1
+            if dp[n][m]!=-1:
+                return dp[n][m]
+            if text1[n] == text2[m]:
+                dp[n][m] = solve(n+1,m+1)+1
             else:
-                dp[i][j] = max(solve(i+1,j), solve(i,j+1))
-            return dp[i][j]
+                dp[n][m] = max(solve(n+1,m),solve(n,m+1))
+            return dp[n][m]
         return solve(0,0)
