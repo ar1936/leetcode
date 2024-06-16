60d415c8a41cdd79840d3b753fb1c1288960a356
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-06-16 12:37:34 +0530

    Time: 45 ms (83.46%) | Memory: 21.1 MB (18.40%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,41 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # def solve(root,ans,tmp):
-        #     if root is None:
-        #         return 
-        #     tmp.append(root.val)
-        #     if root.val == p.val or root.val == q.val:
-        #         ans.append(tmp.copy())
-            
-        #     solve(root.left,ans,tmp)
-        #     solve(root.right,ans,tmp)
-        #     tmp.pop()
-        # ans = []
-        # solve(root,ans,[])
-        # prev = -1
-        # for i in range(len(ans[0])):
-        #     if ans[0][i] == ans[1][i]:
-        #         prev = ans[0][i]
-        #     else:
-        #         break
-        # return TreeNode(prev)
-        # ans = [None]
-        def solve(root,ans):
-            if root is None:
-                return None
-            if root == p or root==q:
-                ans[0] = root
-                return root
-            left = solve(root.left,ans)
-            right = solve(root.right,ans)
-            if left is not None and right is not None:
-                ans[0] = root
-            return left if left is not None else right
-
-        if root is None:
-            return root
-        ans = [None]
-        solve(root,ans)
-        return ans[0]
+        def lca(node,start,end):
+            if node is None or node == start or node == end:
+                return node
+            left = lca(node.left,start,end)
+            right = lca(node.right,start,end)
+            if left and right:
+                return node
+            return left if left else right
+        return lca(root,p,q)
 
