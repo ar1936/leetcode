16e9deca9cbf993027eb0d4ff62f1145858951a9
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-07-05 21:01:27 +0530

    Time: 598 ms (9.48%) | Memory: 184.4 MB (6.60%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -1,11 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.next = defaultdict(TrieNode)
+        self.is_finish = False
+
 class Solution:
-    def replaceWords(self, roots, sentence):
-        rootset = set(roots)
+    def __init__(self):
+            self.root = TrieNode()
+
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        def insert(word):
+            curr = self.root 
+            for c in word:
+                curr = curr.next[c]
+            curr.is_finish = True
 
         def replace(word):
-            for i in range(1, len(word)):
-                if word[:i] in rootset:
-                    return word[:i]
-            return word
+            curr = self.root
+            match = "" 
+            for c in word:
+                if c in curr.next:
+                    match += c
+                curr = curr.next[c]
+                if curr.is_finish == True:
+                    return match 
+            return ""
 
-        return " ".join(map(replace, sentence.split()))
+        for d in dictionary:
+            insert(d)
+        
+        ans = ""
+        sentence = sentence.split()
+
+        for s in sentence:
+            replace_word = replace(s)
+            if replace_word != "":
+                ans += replace_word
+            else:
+                ans+= s
+            ans+=' '
+        return ans[:-1]
