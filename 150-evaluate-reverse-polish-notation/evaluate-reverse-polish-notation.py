7850aafd80355f1ec9c99382fcedc0bcf0405bf5
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-07-13 22:49:41 +0530

    Time: 66 ms (53.39%) | Memory: 17.1 MB (57.38%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,21 +1,18 @@
 class Solution:
-    def evalRPN(self, nums: List[str]) -> int:
-        s = []
-        if len(nums)==1:
-            return int(nums[0])
-        for i in range(len(nums)):
-            s.append(nums[i])
-            while(s[-1] in ['+','-','/','*']):
-                sign = s.pop()
-                num2 = int(s.pop())
-                num1 = int(s.pop())
-                if sign == '+':
-                    tmp_ans = num1+num2
-                elif sign == '-':
-                    tmp_ans = num1 - num2
-                elif sign == '*':
-                    tmp_ans = num1*num2
-                else:
-                    tmp_ans = int(num1/num2)
-                s.append(tmp_ans)
-        return s[-1]
+    def evalRPN(self, s: List[str]) -> int:
+        st = []
+        for c in s:
+            if c in ['+', '-', "/", "*"]:
+                num1 = st.pop()
+                num2 = st.pop()
+                if c == '+':
+                    st.append(num1+num2)
+                if c == '-':
+                    st.append(num2-num1)
+                if c == '*':
+                    st.append(num1*num2)
+                if c == '/':
+                    st.append(int(num2/num1))
+            else:
+                st.append(int(c))
+        return st.pop()
