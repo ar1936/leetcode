472d1e9147f8c530c4c3dfd72286fb62bad6aa8b
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-07-22 21:20:34 +0530

    Time: 403 ms (11.35%) | Memory: 33.6 MB (5.85%) - LeetSync

---
+++ 971-shortest-bridge/shortest-bridge.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        dx = [0,0,-1,1]
+        dy = [1,-1,0,0]
+        n = len(grid)
+        m = len(grid[0])
+        island_ind = set()
+        def dfs(x,y):
+            if x<0 or x>=n or y<0 or y>=m or grid[x][y] ==0:
+                return 
+            grid[x][y] = 0
+            island_ind.add((x,y))
+            for i in range(4):
+                dfs(x+dx[i],y+dy[i])
+            
+        for i in range(n):
+            flag = False
+            for j in range(m):
+                if grid[i][j] == 1:
+                    dfs(i,j)
+                    flag = True
+                    break
+            if flag:
+                break
+                
+        ans = 10000000
+        q = deque()
+        for (x,y) in island_ind:
+            q.append((x,y,0))
+        while q:
+            r,c, cnt= q.popleft()
+            if grid[r][c]==1:
+                ans = min(cnt,ans)
+            for i in range(4):
+                new_x, new_y = r+dx[i], c + dy[i]
+                if new_x>=0 and new_x<n and new_y>=0 and new_y<m and (new_x,new_y) not in island_ind:
+                    q.append((new_x,new_y,cnt+1))
+                    island_ind.add((new_x,new_y))
+
+        return ans - 1
