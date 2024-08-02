b69d643762a94ad47e70dbd0a95452750fec54b1
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-02 22:27:11 +0530

    Time: 654 ms (91.89%) | Memory: 20.6 MB (37.84%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,17 +1,29 @@
 class Solution:
-    def minSwaps(self, nums: List[int]) -> int:
-        gap = sum(nums)  # Total number of 1's in the array
-        if gap == 0:  # If there are no 1's, no swaps are needed
-            return 0
+    # def minSwaps(self, nums: List[int]) -> int:
+    #     gap = sum(nums)  # Total number of 1's in the array
+    #     if gap == 0:  # If there are no 1's, no swaps are needed
+    #         return 0
         
+    #     n = len(nums)
+    #     nums = nums + nums  # Extend the array to handle the circular nature
+    #     tmp = sum(nums[:gap])  # Sum of the first 'gap' elements (initial window sum)
+    #     ans = gap - tmp  # Initial number of swaps needed
+        
+    #     # Slide the window across the array
+    #     for i in range(1, n):
+    #         tmp += nums[i + gap - 1] - nums[i - 1]
+    #         ans = min(ans, gap - tmp)
+        
+    #     return ans
+
+   def minSwaps(self, nums: List[int]) -> int:
+        gap = sum(nums)
         n = len(nums)
-        nums = nums + nums  # Extend the array to handle the circular nature
-        tmp = sum(nums[:gap])  # Sum of the first 'gap' elements (initial window sum)
-        ans = gap - tmp  # Initial number of swaps needed
-        
-        # Slide the window across the array
-        for i in range(1, n):
-            tmp += nums[i + gap - 1] - nums[i - 1]
-            ans = min(ans, gap - tmp)
-        
+        tmp = sum(nums[:gap])
+        ans = gap -  tmp 
+        # nums.extend(nums)
+        for i in range(n):
+            tmp+=(nums[(gap+i)%n]-nums[i])
+            
+            ans = min(gap-tmp, ans)
         return ans
