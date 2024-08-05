7a649d521271c9aac307c7a078012031653c7ef0
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-05 21:36:27 +0530

    Time: 575 ms (72.15%) | Memory: 25.4 MB (92.18%) - LeetSync

--- 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.py
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.py
@@ -1,36 +1,35 @@
 class Solution:
     def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
-        def topo_sort(arr,n):
-            graph = [[] for _ in range(n+1)]
-            indegree = [0]*(n+1)
-            for a,b in arr:
-                graph[a].append(b)
-                indegree[b] += 1
+        def topology_sort(nums):
+            graph = defaultdict(list)
+            indegree = defaultdict(int)
+            ans = []
+            for n1, n2 in nums:
+                graph[n1].append(n2)
+                indegree[n2] += 1
             q = deque()
-            for k in range(1,n+1):
-                if indegree[k]==0:
-                    q.append(k)
-            topo_sort = []
+            for key in range(1,k+1):
+                if indegree[key] == 0:
+                    q.append(key)
             while q:
-                prev_node = q.popleft()
-                topo_sort.append(prev_node)
-                n-=1
-                for curr_node in graph[prev_node]:
-                    indegree[curr_node]-=1
-                    if indegree[curr_node]==0:
-                        q.append(curr_node)
-            if n!=0:
-                return False
-            return topo_sort
-
-        row_topo = topo_sort(rowConditions,k)
-        col_topo = topo_sort(colConditions,k)
-        if row_topo == False or col_topo == False:
-            return []
+                node = q.popleft()
+                ans.append(node)
+                for next_node in graph[node]:
+                    indegree[next_node] -= 1
+                    if indegree[next_node] == 0:
+                        q.append(next_node)
+            if len(ans)!=k:
+                return []
+            return ans
         
-        matrix = [[0] * k for _ in range(k)]
+        row_topology = topology_sort(rowConditions)
+        col_topology = topology_sort(colConditions)
+        if row_topology == [] or col_topology == []:
+            return []
+        mat = [[0]*k for _  in range(k)]
+
         for i in range(k):
             for j in range(k):
-                if row_topo[i] == col_topo[j]:
-                    matrix[i][j] = row_topo[i]
-        return matrix
+                if row_topology[i] == col_topology[j]:
+                    mat[i][j] = row_topology[i]
+        return mat
