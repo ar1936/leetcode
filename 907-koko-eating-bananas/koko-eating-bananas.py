992ae8546d521782feeb05b18bd7a2a132aac16f
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-11 18:04:25 +0530

    Time: 304 ms (21.39%) | Memory: 18.1 MB (83.69%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,16 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def solve(mid):
-            sum = 0
+        l = 1
+        r = max(piles)
+        ans = 0
+        def solve(m):
+            count = 0 
             for p in piles:
-                sum+=(p//mid + int(p%mid!=0))
-            return sum<=h
-        low = 1
-        high = max(piles)
-        while low<=high:
-            mid = low + (high-low)//2
-            if solve(mid):
-                high = mid-1
+                if p<=m:
+                    count+=1
+                else:
+                    count += p//m
+                    if p%m!=0:
+                        count += 1
+            return count<=h
+        while l<=r:
+            mid = (l+r)//2
+            if(solve(mid)):
+                r = mid - 1
+                ans = mid  
             else:
-                low = mid+1
-        return low
+                l = mid + 1
+        return ans
