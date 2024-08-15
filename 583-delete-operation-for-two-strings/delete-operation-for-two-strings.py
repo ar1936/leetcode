5b2b1358ccfbf24fabdc63571fcce8183a847d2a
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-15 21:59:58 +0530

    Time: 153 ms (77.59%) | Memory: 20.3 MB (27.44%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -1,20 +1,16 @@
 class Solution:
-    def minDistance(self, s1: str, s2: str) -> int:
-        n = len(s1)
-        m = len(s2)
-        dp = [[-1]*m for _ in range(n)]
-        def solve(i,j):
-            if i == n and j == m:
-                return 0
-            if i == n :
-                return m - j
-            if j == m :
-                return n - i
-            if dp[i][j] != -1:
-                return dp[i][j]
-            if s1[i] == s2[j]:
-                dp[i][j] = solve(i+1,j+1)
-            else:
-                dp[i][j] =  1 + min(solve(i+1,j), solve(i, j+1))
-            return dp[i][j]
-        return solve(0,0)
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0] = i
+        for j in range(m+1):
+            dp[0][j] = j
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1])+1
+        return dp[n][m]
