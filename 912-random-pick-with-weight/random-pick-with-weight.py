a17cc375154b63fd0915d5c6ff3375c6191825b9
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-16 00:30:20 +0530

    Time: 128 ms (92.72%) | Memory: 21.1 MB (49.18%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -6,18 +6,12 @@ class Solution:
             if i == 0:
                 self.pre_sum.append(w[i])
             else:
-                self.pre_sum.append(w[i]+self.pre_sum[-1])
-             
+                self.pre_sum.append(self.pre_sum[-1] + w[i])
+            
     def pickIndex(self) -> int:
-        val = random.random() * self.pre_sum[-1]
-        # ans = 0
-        # for i in range(len(self.pre_sum)):
-        #     if self.pre_sum[i]>val:
-        #         return i
-        # return -1
-        return bisect_right(self.pre_sum, val)
-
-
+        sum = random.random()*self.pre_sum[-1]
+        ind = bisect_left(self.pre_sum,sum)
+        return ind
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
