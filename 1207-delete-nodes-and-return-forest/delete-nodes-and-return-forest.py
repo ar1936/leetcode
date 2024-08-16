6787d6921270faa937eaad0409267a236ca35e88
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-16 21:22:17 +0530

    Time: 57 ms (44.33%) | Memory: 17 MB (78.82%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.py
@@ -6,21 +6,24 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        self.ans = []
-        def solve(node,st):
+        ans = []
+        to_delete = set(to_delete)
+        def solve(node):
             if node is None:
                 return None
-            node.left = solve(node.left,st)
-            node.right = solve(node.right,st)
-            if node.val in st:
-                if node.left is not None:
-                    self.ans.append(node.left)
-                if node.right is  not None:
-                    self.ans.append(node.right)
+            node.left = solve(node.left)
+            node.right = solve(node.right)
+
+            if node.val in to_delete:
+                if node.left:
+                    ans.append(node.left)
+                if node.right:
+                    ans.append(node.right)
                 return None
             return node
-        st = set(to_delete)
-        root = solve(root,st)
+        if root is None:
+            return None
+        root = solve(root)
         if root is not None:
-            self.ans.append(root)
-        return self.ans
+            ans.append(root)
+        return ans
