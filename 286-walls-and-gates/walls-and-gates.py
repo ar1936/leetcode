0bdf8efbe89bcc63459f6f479bfc39cce3d30ded
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-19 01:33:07 +0530

    Time: 199 ms (45.68%) | Memory: 23 MB (32.24%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,25 +3,24 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        n = len(rooms)
-        m = len(rooms[0])
-        dx = [0,0,-1,1]
-        dy = [1,-1,0,0]
+        dx = [0,0,1,-1]
+        dy = [-1,1,0,0]
         q = deque()
         visited = set()
+        n = len(rooms)
+        m = len(rooms[0])
         for i in range(n):
             for j in range(m):
                 if rooms[i][j] == 0:
-                    q.append((i,j,0))
+                    q.append((i,j, 0))
                     visited.add((i,j))
         
         while q:
-            x,y, step = q.popleft()
+            x,y,d = q.popleft()
+            rooms[x][y] = d
             for k in range(4):
-                new_x,new_y, new_step = x+dx[k] , y + dy[k], step + 1
-                if 0<=new_x<n and 0<=new_y<m and rooms[new_x][new_y] != -1:
-                    if (new_x,new_y) not in visited:
-                        visited.add((new_x,new_y))
-                        q.append((new_x,new_y,new_step))
-                        rooms[new_x][new_y] = new_step
+                curr_x, curr_y = x + dx[k], y + dy[k]
+                if 0<=curr_x<n and 0<=curr_y<m and (curr_x,curr_y) not in visited and rooms[curr_x][curr_y] != -1:
+                    visited.add((curr_x,curr_y))
+                    q.append((curr_x, curr_y, d+1))
         return rooms
