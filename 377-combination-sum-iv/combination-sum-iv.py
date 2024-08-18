6b6a460641bac56c4b163e599078547a7e1165a4
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-18 15:48:52 +0530

    Time: 45 ms (27.19%) | Memory: 16.6 MB (19.75%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -2,17 +2,21 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [-1]*10000000
-        def backtrack(current_sum: int):
+        dp = [None] * (target + 1)
+        
+        def backtrack(current_sum: int) -> int:
             if current_sum == target:
                 return 1
             if current_sum > target:
-                return 0 
-            if dp[current_sum] !=-1:
+                return 0
+            if dp[current_sum] is not None:
                 return dp[current_sum]
-            tmp = 0
-            for i in range(len(nums)):
-               tmp += backtrack(current_sum + nums[i])
-            dp[current_sum] = tmp
+            
+            total_combinations = 0
+            for num in nums:
+                total_combinations += backtrack(current_sum + num)
+            
+            dp[current_sum] = total_combinations
             return dp[current_sum]
+        
         return backtrack(0)
