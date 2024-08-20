d2b0178d3fa2874bed86a6c57446a4ed1df42b22
Ashish Ranjan <70103080+ar1936@users.noreply.github.com>
2024-08-20 21:45:45 +0530

    Time: 1372 ms (10.41%) | Memory: 46.6 MB (44.05%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.py
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -1,25 +1,24 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        indegree = [0]*(n)
-        mp = defaultdict(list)
+        indegree = [0]*n 
+        graph = defaultdict(list)
+        dist = time[:]
         for r1, r2 in relations:
-            mp[r1-1].append(r2-1)
-            indegree[r2-1] += 1
-        heap = []
+            graph[r1-1].append((r2-1,time[r2-1]))
+            indegree[r2-1]+=1
         ans = time[:]
-        cnt = 0
+        heap = []
         for i in range(n):
             if indegree[i] == 0:
-                heapq.heappush(heap,(time[i],i))
-
+                heapq.heappush(heap, (time[i], i))
+        
         while heap:
-            prev_val, prev_from = heapq.heappop(heap)
-            for curr_node in mp[prev_from]:
-                total_time = time[curr_node] + prev_val
-                indegree[curr_node]-=1
+            prev_time, prev_node = heapq.heappop(heap)
+            for curr_node, curr_time in graph[prev_node]:
+                indegree[curr_node] -= 1
+                total_time = prev_time + curr_time
                 if ans[curr_node]<total_time:
                     ans[curr_node] = total_time
                 if indegree[curr_node] == 0:
-                    heapq.heappush(heap,(total_time,curr_node))
-        
+                    heapq.heappush(heap, (total_time, curr_node)) 
         return max(ans)
